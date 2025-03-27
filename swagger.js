9e0a81851e869aa48013052e9fecf0e8e053dc40
bnewvillage <blainenewvillage@gmail.com>
2025-03-27 17:02:11 +0400

    removed http

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'CSE 341 Project 2 API',
     description: 'Project 2'
   },
-  host: 'https://cse341-4p35.onrender.com'
+  host: 'cse341-4p35.onrender.com'
 };
 
 const outputFile = './swagger.json';
